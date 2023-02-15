0e8d5dce1ec5ec515f5c079efa08fe1cfa8ccb27
Varaidzosmangezi <mangezivaraidzos@gmail.com>
2023-02-15 11:03:03 +0200

    commit 4

--- javascript/script.js
+++ javascript/script.js
@@ -6,127 +6,161 @@ const equalToButton = document.querySelector('.equal-to');
 const currentOperandDisplay = document.querySelector('.current-operand')
 const previousOperandDisplay = document.querySelector('.previous-operand')
 
-class Calculator {
-    constructor(previousOperandDisplay, currentOperandDisplay) {
-        this.previousOperandDisplay = previousOperandDisplay;
-        this.currentOperandDisplay = currentOperandDisplay;
-        this.allClear()
-    }
-    allClear () {
-        this.currentOperand = '';
-        this.previousOperand = '';
-        this.operator = undefined;
+let currentOperand = '';
+let previousOperand = '';
+let operation = '';
 
-    }
-    appendNumber (number) {
-        if (number === '.' && this.currentOperand.includes('.'))return;
-        this.currentOperand += number;
-    }
-    chooseOperator (operator) {
-        if (this.currentOperand ==='') {
-            return;
-        }
-        if (this.previousOperand !== '') {
-            this.operate ();
-        }
-        this.operator = operator;
-       if (this.previousOperand.includes(operator))return
-        this.previousOperand = this.currentOperand + this.operator;
-        this.currentOperand = ''
-    }
-    
-    operate (answer) {
-        const floatCurrentOperand = parseFloat(this.currentOperand);
-        const floatPreviousOperand = parseFloat(this.previousOperand);
-        (floatCurrentOperand , floatPreviousOperand)
-        if (isNaN(floatPreviousOperand) || isNaN(floatCurrentOperand)) return
-            switch (this.operator) {
-                case '+':
-                    this.addition(floatPreviousOperand,floatCurrentOperand);
-                    (floatCurrentOperand, floatPreviousOperand)
-                    break;
-                case '-':
-                    this.subtraction(floatPreviousOperand,floatCurrentOperand);
-                    break;
-                case '*':
-                    this.multiplication(floatPreviousOperand,floatCurrentOperand);
-                    break;
-                case '/':
-                    if (floatCurrentOperand === 0){
-                        this.currentOperand = `Oh no! you can't divide by zero`;
-                        this.previousOperand = '';
-                        this.operator = undefined;
-                        return
-                    }
-                    this.division (floatPreviousOperand,floatCurrentOperand);
-                    break;
-                default:
-                    return
-        }
-        this.previousOperand = ''
-        this.operation = undefined;
-    }
-
-    addition(a,b) {
-        let answer = 0
-        answer = a + b;
-        this.currentOperand = answer;
-    }
-    subtraction (a,b) {
-        let answer = 0
-        answer = a - b;
-        this.currentOperand = answer;
-    }
-    multiplication (a,b) {
-        let answer = 0
-        answer = a * b;
-        this.currentOperand = answer;
-    }
-    division (a,b) {
-        if (b === 0) return;
-        let answer = 0
-        answer = a / b;
-        this.currentOperand = answer;
-    }
-
-    updateDisplay (){
-        currentOperandDisplay.textContent = this.currentOperand;
-        previousOperandDisplay.textContent = this.previousOperand ;
-    }
-    
-    delete () {
-        this.currentOperand = this.currentOperand.toString().slice(0,-1);
-
-    }
+function appendNumber (number) {
+    if(number === '.'&& currentOperand.includes('.') ) return
+    if(currentOperand[0] === '0' && number ==='0' && currentOperand.length === 1) return
+    if(currentOperand.length > 12)return
+    let appendedNum = currentOperand + number;
+    currentOperand = appendedNum;
+    return currentOperand
 }
-const calculator = new Calculator ();
+function chooseOperation (operator) {
+    if (currentOperand === '' && previousOperand === '' && operation === '') return
+    if (operation !== ''&& previousOperand !== ''  && currentOperand === ''){
+        previousOperand = previousOperand.slice(0,-1) + operator;
+        currentOperand ='';
+        operation = operator;
+        return
+    }
+    if (previousOperand === ''&& operation === ''  ) {
+        previousOperand = currentOperand + operator;
+        currentOperand = '';
+        operation = operator;
+        return
+        }
+    if ( previousOperand !== '' && currentOperand !=='' &&operation !== ''){
+        operate ();
+        operation = operator
+        previousOperand = currentOperand + operator;
+        if (currentOperand === `Oops! you can't divide by zero`){
+            previousOperand = '';
+            operation = ''
+            return
+        }
+        else {
+            currentOperand = '';
+            operation = operator;
+            return
+        }
+    }
+
+}
+function operate (){
+    const floatCurrentOperand = parseFloat(currentOperand);
+    const floatPreviousOperand = parseFloat(previousOperand);
+    if (isNaN(floatCurrentOperand) || isNaN(floatPreviousOperand)) return
+    switch (operation){
+        case '+':
+            addition (floatPreviousOperand, floatCurrentOperand);
+            break;
+        case '-':
+            subtraction(floatPreviousOperand, floatCurrentOperand);
+            break;
+        case '*':
+            multiplication(floatPreviousOperand, floatCurrentOperand);
+            break;
+        case '/':
+            if (floatCurrentOperand === 0) {
+                currentOperand = `Oops! you can't divide by zero`;
+                previousOperand ='';
+                operation = '';
+                return
+            }
+            else{
+                division(floatPreviousOperand, floatCurrentOperand);
+            break;
+            }
+        default:
+            break;
+    }
+    previousOperand = '';
+    operation = '';
+}
+
+function updateDisplay () {
+    currentOperandDisplay.textContent = displayFormat(currentOperand);
+    previousOperandDisplay.textContent = displayFormat(previousOperand) + operation;
+}
+
+function addition (a,b) {
+    let answer = a + b;
+    currentOperand = answer;
+    return currentOperand
+
+}
+function subtraction (a,b) {
+    let answer = a - b;
+    currentOperand = answer;
+    return currentOperand;
+}
+function multiplication (a,b) {
+    let answer = a * b;
+    currentOperand = answer;
+    return currentOperand;
+}
+function division (a,b) {
+        let answer = a / b;
+        currentOperand = answer;
+}
+function allClear (){
+    currentOperand = '';
+    previousOperand = '';
+    operation ='';
+}
+function deleteNum (){
+    currentOperand = currentOperand.slice(0,-1);
+}
+function displayFormat (value) {
+    if (value === `Oops! you can't divide by zero`){
+        let numberToDisplay = `Oops! you can't divide by zero`
+        return numberToDisplay
+    }
+    else{
+        let valueValue = parseFloat(value).toFixed(4);
+        numberToDisplay = new Intl.NumberFormat().format(valueValue);
+        console.log(numberToDisplay);
+        if (numberToDisplay === 'NaN') {
+            numberToDisplay = '';
+            return numberToDisplay;
+        }
+        return numberToDisplay;
+    }
+    }
+
 
 digitButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.id)
-        calculator.updateDisplay ();
+        appendNumber(button.id);
+        updateDisplay();
+        console.log(currentOperand+typeof(currentOperand), previousOperand + typeof (previousOperand));
+
     })
 })
 
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.chooseOperator(button.id)
-        calculator.updateDisplay ();
+        chooseOperation(button.id);
+        updateDisplay();
+        console.log(currentOperand+typeof(currentOperand), previousOperand + typeof (previousOperand))
+
     })
 })
 
 
 equalToButton.addEventListener('click', button => {
-    calculator.operate ();
-    calculator.updateDisplay ();;
+    operate();
+    updateDisplay();
     })
 
 allClearButton.addEventListener('click', button => {
-    calculator.allClear ();
-    calculator.updateDisplay ();
+    allClear();
+    updateDisplay ();
     })
-
 deleteButton.addEventListener('click', button => {
-    calculator.delete ();
-    calculator.updateDisplay ();   
-    })
+    deleteNum ();
+    updateDisplay ();
+})
